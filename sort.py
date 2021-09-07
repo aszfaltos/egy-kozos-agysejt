0103e399d3cf6276102f4c8657427795402afbf8
András Tóth <76177923+tandrast@users.noreply.github.com>
2021-09-07 19:33:25 +0200

    updated sort.py

--- sort.py
+++ sort.py
@@ -6,12 +6,13 @@ import re
 src = "./.upload/"
 
 # dist dirs - files will be sorted into these dirs
-# var name is the three character id of the subject
+# var name is the id of the subject
 buf = "./business-fundamentals/"
-pro = "./programozas_eloadas/"
+pro = "./programozas-eloadas/"
+progy = ".programozas-gyak/"
 szr = "./szamrend/"
-imp = "./imperativ_eloadas/"
-# TODO: add impgy and progy
+imp = "./imperativ-eloadas/"
+impgy = "./imperativ-gyak/"
 
 # loads filenames in src dir to memory
 for path, subdirs, files in os.walk(src):
@@ -20,7 +21,7 @@ for path, subdirs, files in os.walk(src):
         # creates the path for the src file
         srcpath = path + filename
 
-        # checks the three cararacter prefix
+        # checks the prefix
         if re.search("^buf", filename):
             # copies the file and removes the prefix
             shutil.copyfile(srcpath, buf + filename.replace("buf_", ""))
@@ -30,6 +31,10 @@ for path, subdirs, files in os.walk(src):
         if re.search("^pro", filename):
             shutil.copyfile(srcpath, pro + filename.replace("pro_", ""))
             os.remove(srcpath)
+            
+        if re.search("^progy", filename):
+            shutil.copyfile(srcpath, progy + filename.replace("progy_", ""))
+            os.remove(srcpath)
 
         if re.search("^szr", filename):
             shutil.copyfile(srcpath, szr + filename.replace("szr_", ""))
@@ -37,4 +42,8 @@ for path, subdirs, files in os.walk(src):
 
         if re.search("^imp", filename):
             shutil.copyfile(srcpath, imp + filename.replace("imp_", ""))
+            os.remove(srcpath)
+            
+        if re.search("^impgy", filename):
+            shutil.copyfile(srcpath, impgy + filename.replace("impgy_", ""))
             os.remove(srcpath)
