dd5872addb93eb2007c9cf9182c6f1810c86aa57
tandrast <toth.andras.tamas@protonmail.com>
2021-10-12 15:08:03 +0200

    gyak6 1.0.1

--- imperativ-gyak/aszfalt_codes/gyak6.c
+++ imperativ-gyak/aszfalt_codes/gyak6.c
@@ -13,12 +13,12 @@ typedef struct
 	float price;
 } Product;
 
-Product *find(int const length, Product const *const product, enum Size searchFor);
+Product const *find(int const length, Product const *const product, enum Size const searchFor);
 
 int main()
 {
-	Product prods[3] = {{Small, 12.5f},
-											{Medium, 4.5f},
+	Product prods[3] = {{Medium, 12.5f},
+											{Small, 4.5f},
 											{Small, 6.5f}};
 	Product *prod = find(3, prods, Small);
 	printf("Size: %d, price: %f\n", prod->size, prod->price);
@@ -26,14 +26,21 @@ int main()
 	return 0;
 }
 
-Product *find(int const length, Product const *const product, enum Size searchFor)
+Product const *find(int const length, Product const *const product, enum Size const searchFor)
 {
-	int minI = 0;
+	int minI = -1;
 	for (int i = 0; i < length; i++)
 	{
-		if (product[i].size == searchFor && product[minI].price > product[i].price)
+		if (product[i].size == searchFor)
 		{
-			minI = i;
+			if (minI < 0)
+			{
+				minI = i;
+			}
+			if (minI >= 0 && product[minI].price > product[i].price)
+			{
+				minI = i;
+			}
 		}
 	}
 
